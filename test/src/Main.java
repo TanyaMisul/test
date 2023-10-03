c73ae79cff80c87e4d7549435c1cf453e897b553
Tanya <tanya.misyul1973@mail.ru>
2023-10-03 14:34:40 +0300

    ta

--- test/src/Main.java
+++ test/src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("com");
         System.out.println("tanya");
+        System.out.println("567");
     }
 }
